eab9dfb1cd1f9aa1598422b6ab862cd2039be0fc
Chris Oliver <excid3@gmail.com>
2015-07-27 14:41:56 -0400

    Add a few more postgres types to exclude

--- lib/postgres_to_redshift/column.rb
+++ lib/postgres_to_redshift/column.rb
@@ -52,6 +52,10 @@ class PostgresToRedshift::Column
     "bytea" => "CHARACTER VARYING(65535)",
     "money" => "DECIMAL(19,2)",
     "oid" => "CHARACTER VARYING(65535)",
+    "timestamp with time zone" => "TIMESTAMP",
+    "hstore" => "CHARACTER VARYING(65535)",
+    "USER-DEFINED" => "CHARACTER VARYING(65535)",
+    "ARRAY" => "CHARACTER VARYING(65535)",
   }
 
   def initialize(attributes: )
